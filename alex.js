5dd8a18482b9bf1fcdfccabf085b45ee122e5519
Liu Yang <zation1@gmail.com>
2014-08-09 08:59:05 +0800

    change the text when view details

--- alex.js
+++ alex.js
@@ -2,11 +2,17 @@
 
   var $collapser = $('.expand-button .collapser');
   $collapser.toggle(function() {
-    var $this = $(this);
+    var $this = $(this),
+      $text = $this.find('.collapser-text');
+
+    $text.text($text.data('opened-text'));
     $this.find('.icon-uniC100').removeClass('icon-uniC100').addClass('icon-uniC800');
     $this.parents('.expand-button').siblings('.collapsible').slideDown();
   }, function() {
-    var $this = $(this);
+    var $this = $(this),
+      $text = $this.find('.collapser-text');
+
+    $text.text($text.data('closed-text'));
     $this.find('.icon-uniC800').removeClass('icon-uniC800').addClass('icon-uniC100');
     $this.parents('.expand-button').siblings('.collapsible').slideUp();
   });
