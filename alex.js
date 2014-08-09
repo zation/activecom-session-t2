bd5b0ce286fd04bedb2d453b57aa11d10a34f1c7
Liu Yang <zation1@gmail.com>
2014-08-09 08:33:35 +0800

    fix some font icon

--- alex.js
+++ alex.js
@@ -2,9 +2,13 @@
 
   var $collapser = $('.expand-button .collapser');
   $collapser.toggle(function() {
-    $(this).parents('.expand-button').siblings('.collapsible').slideDown();
+    var $this = $(this);
+    $this.find('.icon-uniC100').removeClass('icon-uniC100').addClass('icon-uniC800');
+    $this.parents('.expand-button').siblings('.collapsible').slideDown();
   }, function() {
-    $(this).parents('.expand-button').siblings('.collapsible').slideUp();
+    var $this = $(this);
+    $this.find('.icon-uniC800').removeClass('icon-uniC800').addClass('icon-uniC100');
+    $this.parents('.expand-button').siblings('.collapsible').slideUp();
   });
 
 })($);
